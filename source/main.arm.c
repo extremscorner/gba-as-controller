f72b5e8cbb11816c0bb8471a0f5c42c7e174c663
Extrems <metaradil@gmail.com>
2018-04-15 11:52:19 -0400

    Updates.

--- source/main.arm.c
+++ source/main.arm.c
@@ -12,28 +12,53 @@
 
 //#define ANALOG
 
-static uint8_t id[] = {0x29, 0x00, 0x00};
+enum {
+	CMD_ID = 0x00,
+	CMD_STATUS = 0x40,
+	CMD_ORIGIN,
+	CMD_RECALIBRATE,
+	CMD_STATUS_LONG,
+	CMD_RESET = 0xFF
+};
+
+enum {
+	MOTOR_STOP = 0,
+	MOTOR_RUMBLE,
+	MOTOR_STOP_HARD
+};
+
+struct buttons {
+	uint16_t a          : 1;
+	uint16_t b          : 1;
+	uint16_t x          : 1;
+	uint16_t y          : 1;
+	uint16_t start      : 1;
+	uint16_t get_origin : 1;
+	uint16_t err_latch  : 1;
+	uint16_t err_status : 1;
+	uint16_t left       : 1;
+	uint16_t right      : 1;
+	uint16_t down       : 1;
+	uint16_t up         : 1;
+	uint16_t z          : 1;
+	uint16_t r          : 1;
+	uint16_t l          : 1;
+	uint16_t use_origin : 1;
+};
 
 static struct {
-	struct {
-		uint16_t a          : 1;
-		uint16_t b          : 1;
-		uint16_t x          : 1;
-		uint16_t y          : 1;
-		uint16_t start      : 1;
-		uint16_t get_origin : 1;
-		uint16_t err_latch  : 1;
-		uint16_t err_status : 1;
-		uint16_t left       : 1;
-		uint16_t right      : 1;
-		uint16_t down       : 1;
-		uint16_t up         : 1;
-		uint16_t z          : 1;
-		uint16_t r          : 1;
-		uint16_t l          : 1;
-		uint16_t use_origin : 1;
-	} buttons;
+	uint8_t type[2];
 
+	struct {
+		uint8_t mode   : 3;
+		uint8_t motor  : 2;
+		uint8_t origin : 1;
+		uint8_t        : 2;
+	} status;
+} id;
+
+static struct {
+	struct buttons buttons;
 	struct { uint8_t x, y; } stick;
 	union {
 		struct {
@@ -64,31 +89,10 @@ static struct {
 			struct { uint8_t a, b; } button;
 		} mode4;
 	};
-} status = {
-	.buttons  = { .use_origin = 1 },
-	.stick    = { 128, 128 },
-};
+} status;
 
 static struct {
-	struct {
-		uint16_t a          : 1;
-		uint16_t b          : 1;
-		uint16_t x          : 1;
-		uint16_t y          : 1;
-		uint16_t start      : 1;
-		uint16_t get_origin : 1;
-		uint16_t err_latch  : 1;
-		uint16_t err_status : 1;
-		uint16_t left       : 1;
-		uint16_t right      : 1;
-		uint16_t down       : 1;
-		uint16_t up         : 1;
-		uint16_t z          : 1;
-		uint16_t r          : 1;
-		uint16_t l          : 1;
-		uint16_t use_origin : 1;
-	} buttons;
-
+	struct buttons buttons;
 	struct { uint8_t x, y; } stick;
 	struct { uint8_t x, y; } substick;
 	struct { uint8_t l, r; } trigger;
@@ -112,6 +116,7 @@ static bool has_motor(void)
 				return false;
 		}
 	}
+
 	return false;
 }
 
@@ -120,65 +125,71 @@ int SIGetCommand(void *buf, unsigned bits);
 
 int IWRAM_CODE main(void)
 {
-	SoundBias(0);
+	REG_IE = IRQ_SERIAL | IRQ_TIMER2 | IRQ_TIMER1 | IRQ_TIMER0;
+	REG_IF = REG_IF;
+
 	REG_RCNT = R_GPIO | GPIO_IRQ | GPIO_SO_IO | GPIO_SO;
-	REG_IE = IRQ_SERIAL | IRQ_TIMER2 | IRQ_TIMER0;
-	REG_TM0CNT_L = -64;
+
+	REG_TM0CNT_L = -67;
+	REG_TM1CNT_H = TIMER_START | TIMER_IRQ | TIMER_COUNT;
+	REG_TM0CNT_H = TIMER_START;
+
+	SoundBias(0);
+	Halt();
 
 	while (true) {
 		int length = SIGetCommand(buffer, sizeof(buffer) * 8 + 1);
 		if (length < 9) continue;
 
+		unsigned buttons     = ~REG_KEYINPUT;
+		origin.buttons.a     = !!(buttons & KEY_A);
+		origin.buttons.b     = !!(buttons & KEY_B);
+		origin.buttons.z     = !!(buttons & KEY_SELECT);
+		origin.buttons.start = !!(buttons & KEY_START);
+		#ifndef ANALOG
+		origin.buttons.right = !!(buttons & KEY_RIGHT);
+		origin.buttons.left  = !!(buttons & KEY_LEFT);
+		origin.buttons.up    = !!(buttons & KEY_UP);
+		origin.buttons.down  = !!(buttons & KEY_DOWN);
+		#endif
+		origin.buttons.r     = !!(buttons & KEY_R);
+		origin.buttons.l     = !!(buttons & KEY_L);
+
 		switch (buffer[0]) {
-			case 0x00:
-			case 0xFF:
+			case CMD_RESET:
+			case CMD_ID:
 				if (length == 9) {
 					if (has_motor()) {
-						id[0] = 0x09;
-						id[1] = 0x00;
-						id[2] = ROM_GPIODATA;
+						id.type[0] = 0x09;
+						id.type[1] = 0x00;
 					} else {
-						id[0] = 0x29;
-						id[1] = 0x00;
-						id[2] = 0x00;
+						id.type[0] = 0x29;
+						id.type[1] = 0x00;
 					}
 
-					SISetResponse(id, sizeof(id) * 8);
+					SISetResponse(&id, sizeof(id) * 8);
 				}
 				break;
-			case 0x40:
+			case CMD_STATUS:
 				if (length == 25) {
-					unsigned buttons     = ~REG_KEYINPUT;
-					status.buttons.a     = !!(buttons & KEY_A);
-					status.buttons.b     = !!(buttons & KEY_B);
-					status.buttons.z     = !!(buttons & KEY_SELECT);
-					status.buttons.start = !!(buttons & KEY_START);
-					#ifndef ANALOG
-					status.buttons.right = !!(buttons & KEY_RIGHT);
-					status.buttons.left  = !!(buttons & KEY_LEFT);
-					status.buttons.up    = !!(buttons & KEY_UP);
-					status.buttons.down  = !!(buttons & KEY_DOWN);
-					#endif
-					status.buttons.r     = !!(buttons & KEY_R);
-					status.buttons.l     = !!(buttons & KEY_L);
+					id.status.mode  = buffer[1];
+					id.status.motor = buffer[2];
 
+					status.buttons = origin.buttons;
+					status.stick.x = origin.stick.x;
+					status.stick.y = origin.stick.y;
 					#ifdef ANALOG
 					if (buttons & KEY_RIGHT)
 						status.stick.x = origin.stick.x + 100;
 					else if (buttons & KEY_LEFT)
 						status.stick.x = origin.stick.x - 100;
-					else
-						status.stick.x = origin.stick.x;
-
 					if (buttons & KEY_UP)
 						status.stick.y = origin.stick.y + 100;
 					else if (buttons & KEY_DOWN)
 						status.stick.y = origin.stick.y - 100;
-					else
-						status.stick.y = origin.stick.y;
 					#endif
 
-					switch (buffer[1]) {
+					switch (id.status.mode) {
 						default:
 							status.mode0.substick.x = origin.substick.x;
 							status.mode0.substick.y = origin.substick.y;
@@ -187,7 +198,7 @@ int IWRAM_CODE main(void)
 							status.mode0.button.a   = (buttons & KEY_A ? 200 : origin.button.a) >> 4;
 							status.mode0.button.b   = (buttons & KEY_B ? 200 : origin.button.b) >> 4;
 							break;
-						case 0x01:
+						case 1:
 							status.mode1.substick.x = origin.substick.x >> 4;
 							status.mode1.substick.y = origin.substick.y >> 4;
 							status.mode1.trigger.l  = (buttons & KEY_L ? 200 : origin.trigger.l);
@@ -195,7 +206,7 @@ int IWRAM_CODE main(void)
 							status.mode1.button.a   = (buttons & KEY_A ? 200 : origin.button.a) >> 4;
 							status.mode1.button.b   = (buttons & KEY_B ? 200 : origin.button.b) >> 4;
 							break;
-						case 0x02:
+						case 2:
 							status.mode2.substick.x = origin.substick.x >> 4;
 							status.mode2.substick.y = origin.substick.y >> 4;
 							status.mode2.trigger.l  = (buttons & KEY_L ? 200 : origin.trigger.l) >> 4;
@@ -203,13 +214,13 @@ int IWRAM_CODE main(void)
 							status.mode2.button.a   = (buttons & KEY_A ? 200 : origin.button.a);
 							status.mode2.button.b   = (buttons & KEY_B ? 200 : origin.button.b);
 							break;
-						case 0x03:
+						case 3:
 							status.mode3.substick.x = origin.substick.x;
 							status.mode3.substick.y = origin.substick.y;
 							status.mode3.trigger.l  = (buttons & KEY_L ? 200 : origin.trigger.l);
 							status.mode3.trigger.r  = (buttons & KEY_R ? 200 : origin.trigger.r);
 							break;
-						case 0x04:
+						case 4:
 							status.mode4.substick.x = origin.substick.x;
 							status.mode4.substick.y = origin.substick.y;
 							status.mode4.button.a   = (buttons & KEY_A ? 200 : origin.button.a);
@@ -217,11 +228,11 @@ int IWRAM_CODE main(void)
 							break;
 					}
 
-					switch (buffer[2]) {
+					switch (id.status.motor) {
 						default:
 							ROM_GPIODATA = 0;
 							break;
-						case 0x01:
+						case MOTOR_RUMBLE:
 							ROM_GPIOCNT  = 1;
 							ROM_GPIODIR  = 1 << 3;
 							ROM_GPIODATA = 1 << 3;
@@ -231,11 +242,11 @@ int IWRAM_CODE main(void)
 					SISetResponse(&status, sizeof(status) * 8);
 				}
 				break;
-			case 0x41:
+			case CMD_ORIGIN:
 				if (length == 9) SISetResponse(&origin, sizeof(origin) * 8);
 				break;
-			case 0x42:
-			case 0x43:
+			case CMD_RECALIBRATE:
+			case CMD_STATUS_LONG:
 				if (length == 25) SISetResponse(&origin, sizeof(origin) * 8);
 				break;
 		}
